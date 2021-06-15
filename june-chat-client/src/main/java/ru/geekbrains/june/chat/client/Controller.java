d4ccc277f1eb10604088b1cd8bdd175efc79ff44
Romans Gainanovs <gainanov@amazon.com>
2021-06-15 21:22:02 +0100

    lesson6 homework, added thread to the client, tested echo server on 2 clients

--- june-chat-client/src/main/java/ru/geekbrains/june/chat/client/Controller.java
+++ june-chat-client/src/main/java/ru/geekbrains/june/chat/client/Controller.java
@@ -1,9 +1,13 @@
 package ru.geekbrains.june.chat.client;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -19,27 +23,45 @@ public class Controller implements Initializable {
     @FXML
     TextField messageField;
 
+    @FXML
+    Button btn;
+
     private Socket socket;
     private DataInputStream in;
     private DataOutputStream out;
 
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        messageField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                btn.setDisable(newValue.trim().length() == 0);
+            }
+        });
+
         try {
+            // окрываем сокет и подключаемся к localhost:8189
             socket = new Socket("localhost", 8189);
+            // оборачиваем входящий поток сокет в DataInputStream для простоты обращения с байтами
             in = new DataInputStream(socket.getInputStream());
+            // оборачиваем исходящий поток сокет в DataOutputStream для простоты обращения с байтами
             out = new DataOutputStream(socket.getOutputStream());
 
+            // создаём новый поток в который обарачиваем ожидание сообщения от сервера
             Thread readThread = new Thread(() -> {
                 try {
                     while (true) {
+                        // ожидаем сообщение от сервера на входящем потоке
                         String inputMessage = in.readUTF();
+                        // добовляем сообщение в текстовое поле
                         chatArea.appendText(inputMessage + "\n");
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             });
+            // запускаем поток
             readThread.start();
         } catch (IOException e) {
             System.out.println("Не возможно подключится к серверу.");
@@ -49,7 +71,9 @@ public class Controller implements Initializable {
 
     public void sendMessage() {
         try {
-            out.writeUTF(messageField.getText());
+            // отправляем в исходящий поток текс из текстовой строки
+            out.writeUTF(messageField.getText().trim());
+            //очищаем текстовую строку
             messageField.clear();
         } catch (IOException e) {
             e.printStackTrace();
--- june-chat-server/src/main/java/ru/geekbrains/june/chat/server/ServerApp.java
+++ june-chat-server/src/main/java/ru/geekbrains/june/chat/server/ServerApp.java
@@ -9,21 +9,33 @@ import java.net.Socket;
 public class ServerApp {
     public static void main(String[] args) {
         try {
+            // открываем сокет на порту 8189
             ServerSocket serverSocket = new ServerSocket(8189);
             System.out.println("Сервер запущен. Ожидаем подключения клиентов.");
 
-            Socket socket = serverSocket.accept();
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-
-            System.out.println("Клиент подключен.");
-
             while (true) {
-                String inputMessage = in.readUTF();
-                System.out.println(inputMessage);
-                out.writeUTF("ECHO: " + inputMessage);
+                // ожидаем подключения клиента
+                Socket client = serverSocket.accept();
+                // создаём отдельный поток для клиента
+                Thread clientThread = new Thread(() -> {
+                    try {
+                        // входной поток клиента
+                        DataInputStream in = new DataInputStream(client.getInputStream());
+                        // выходной поток клиента
+                        DataOutputStream out = new DataOutputStream(client.getOutputStream());
+                        System.out.println("Клиент подключился.");
+                        while (true) {
+                            // ожидаем сообщение от клиента на входном потоке
+                            String inputMessage = in.readUTF();
+                            // отправляем полученное собщение в исходящий поток
+                            out.writeUTF("ECHO: " + inputMessage);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+                clientThread.start();
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
